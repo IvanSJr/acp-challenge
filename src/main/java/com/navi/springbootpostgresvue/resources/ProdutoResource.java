a36c978c4cb216b92f5087778955b006b2148e54
IvanSJr <ivan.junior2706@gmail.com>
2021-06-07 20:10:14 -0300

    Criando Resource para retornar produtos.

--- src/main/java/com/navi/springbootpostgresvue/resources/ProdutoResource.java
+++ src/main/java/com/navi/springbootpostgresvue/resources/ProdutoResource.java
@@ -1,5 +1,19 @@
 package com.navi.springbootpostgresvue.resources;
 
-public class ProdutoResource {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.navi.springbootpostgresvue.entities.Produto;
+
+@RestController
+@RequestMapping
+public class ProdutoResource {
+	
+	@GetMapping
+	public ResponseEntity<Produto> buscaTodosProdutos(){
+		Produto u = new Produto(1L, "Banana", 2.50, "asdas");
+		return ResponseEntity.ok().body(u);
+	}
 }
